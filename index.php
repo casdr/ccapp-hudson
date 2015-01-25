a2383cb863116654787c64f225e27509b08fbe54
Cas de Reuver <me@cas.yt>
2015-01-25 13:59:45 +0100

    Teachers schedule also worky worky

--- index.php
+++ index.php
@@ -4,12 +4,16 @@ require 'classes/curl.php';
 require 'classes/simplehtmldom.php';
 require 'modules/infoweb_main.php';
 require 'modules/infoweb_student.php';
+require 'modules/infoweb_teacher.php';
 \Slim\Slim::registerAutoloader();
 $app = new \Slim\Slim();
 $app->get('/', function () {
   echo 'Hello.';
 });
-$app->get('/test/:id/:week', function ($id, $week) {
+$app->get('/v1/student/schedule/:id/:week', function ($id, $week) {
   print_r(infoweb_student::main($id, $week));
+});
+$app->get('/v1/teacher/schedule/:id/:week', function ($id, $week) {
+  print_r(infoweb_teacher::main($id, $week));
 });
 $app->run();
---
+++ modules/infoweb_teacher.php
@@ -0,0 +1,75 @@
+<?php
+class infoweb_teacher {
+	public static $ref = 3;
+	// Location of the data
+	public static $class_les = array('times'=>0, 'class'=>7, 'room'=>9, 'groups'=>11);
+	public static $class_toets = array('times'=>0, 'class'=>2, 'teacher'=>4, 'room'=>6, 'groups'=>8, 'comment'=>10);
+
+	//Days and times
+	public static $days_les = array('lesma'=>0,'lesdi'=>1,'leswo'=>2,'lesdo'=>3,'lesvr'=>4);
+	public static $days_toets = array('toetsma'=>0,'toetsdi'=>1,'toetswo'=>2,'toetsdo'=>3,'toetsvr'=>4);
+
+	//Remove stuff in hoverinfo
+	public static $hover_remove = array("showHoverInfo('', '", "', this);");
+
+	public static function main($id, $week) {
+		$id = ucfirst(strtolower($id));
+		infoweb_main::setWeek($week);
+		$page = infoweb_main::getWhole($id, 3);
+		return self::getSchedule($page, $week);
+	}
+
+	/**
+	 * Convert the page into a json
+	 * @param  string The page
+	 * @param  integer Week number
+	 * @return array The data
+	 */
+	public static function getSchedule($page='', $week=0) {
+		$classes = array();
+		foreach(self::$days_les as $day)
+			$classes[$day] = array();
+		$ci = 0;
+		$page = str_get_html($page);
+		if($page->find('.roosterdeel', 0) == false) return false;
+		$schedule = $page->find('.roosterdeel', 0);
+		foreach($schedule->find('div [class=les],[class=toets]') as $class) {
+			$wonum = trim(str_replace(range(0,9),'',$class->id));
+			if($class->class == 'les') {
+				$day_int = self::$days_les[$wonum];
+				$class_this = self::$class_les;
+			}
+			if($class->class == 'toets') {
+				$day_int = self::$days_toets[$wonum];
+				$class_this = self::$class_toets;
+			}
+			
+			$lines = explode('<br />', preg_replace('#<a.*?>([^>]*)</a>#i', '$1', str_replace(self::$hover_remove, '', $class->onclick)));
+			$ttimes = explode(' - ', $lines[$class_this['times']]);
+			if($ci != 0 && $oldday == $day_int && isset($classes[$day_int][$ci - 1]) && $classes[$day_int][$ci - 1]['end'] < $ttimes[0]) {
+				foreach($class_this as $key=>$val)
+					$classes[$day_int][$ci][$key] = '';
+				$classes[$day_int][$ci]['times'] = $classes[$day_int][$ci - 1]['end'].' - '.$ttimes[0];
+				$classes[$day_int][$ci]['start'] = $classes[$day_int][$ci - 1]['end'];
+				$classes[$day_int][$ci]['end'] = $ttimes[0];
+				$classes[$day_int][$ci]['canceled'] = false;
+				$classes[$day_int][$ci]['changed'] = false;
+				$classes[$day_int][$ci]['class'] = 'Pauze';
+				$classes[$day_int][$ci]['type'] = 'break';
+				$ci++;
+			}
+			$classes[$day_int][$ci]['day'] = $day_int;
+			foreach($class_this as $key=>$val)
+				$classes[$day_int][$ci][$key] = $lines[$val];
+			$extimes = explode(' - ', $classes[$day_int][$ci]['times']);
+			$classes[$day_int][$ci]['start'] = $extimes[0];
+			$classes[$day_int][$ci]['end'] = $extimes[1];
+			$classes[$day_int][$ci]['canceled'] = false;
+			$classes[$day_int][$ci]['changed'] = false;
+			$classes[$day_int][$ci]['type'] = $class->class;
+			$oldday = $day_int;
+			$ci++;
+		}
+		return $classes;
+	}
+}
