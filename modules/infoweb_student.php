7555bc708290570a509616d0a3f1ca5cdb096ad3
Cas de Reuver <me@cas.yt>
2015-01-31 13:04:11 +0100

    Fixed student schedule

--- modules/infoweb_student.php
+++ modules/infoweb_student.php
@@ -1,9 +1,9 @@
 <?php
 class infoweb_student {
 	public static $ref = 2;
-	//Days and times
-	public static $days_les = array('lesma'=>0,'lesdi'=>1,'leswo'=>2,'lesdo'=>3,'lesvr'=>4);
-	public static $days_toets = array('toetsma'=>0,'toetsdi'=>1,'toetswo'=>2,'toetsdo'=>3,'toetsvr'=>4);
+	//Lines
+	public static $class_les = array('times'=>0, 'teacher'=>7, 'class'=>9, 'room'=>11, 'groups'=>13);
+	public static $class_toets = array('times'=>0, 'class'=>2, 'teacher'=>4, 'room'=>6, 'groups'=>8);
 
 	public static function main($id, $week) {
 		infoweb_main::setWeek($week);
