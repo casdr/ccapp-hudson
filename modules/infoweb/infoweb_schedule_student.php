af34041ded39fe0e77099eba7e1a009ee16736a4
root <root@cas-web-02.casrv.ga>
2015-05-20 20:53:15 +0200

    Tests

--- modules/infoweb/infoweb_schedule_student.php
+++ modules/infoweb/infoweb_schedule_student.php
@@ -3,7 +3,7 @@ class infoweb_student {
 	public static $ref = 2;
 	//Lines
 	public static $class_les = array('times'=>0, 'teacher'=>7, 'class'=>9, 'lesson'=>9, 'room'=>11, 'groups'=>13);
-	public static $class_toets = array('times'=>0, 'class'=>2, 'lesson'=>2, 'teacher'=>4, 'room'=>6, 'groups'=>8);
+	public static $class_toets = array('times'=>0, 'class'=>6, 'lesson'=>8, 'teacher'=>4, 'room'=>2, 'groups'=>8);
 
 	public static function main($id, $week) {
 		infoweb_main::setWeek($week);
