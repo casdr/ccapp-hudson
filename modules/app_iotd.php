2a017404392bd334876267508cad81f3fce8ef42
Cas de Reuver <casdr@0fi.net>
2015-06-02 20:27:34 +0200

    fcking work

--- modules/app_iotd.php
+++ modules/app_iotd.php
@@ -7,7 +7,7 @@ class app_iotd {
 	public static function main() {
 		$html = file_get_html('http://photography.nationalgeographic.com/photography/photo-of-the-day/nature-weather/');
 		$page = str_get_html($html);
-		$link = $page->find('search_results', 0)->find('a', 0)->href;
+		$link = $page->find('#search_results', 0)->find('a', 0)->href;
 
 		$html = str($link);
 		$page = str_get_html($html);
