f02f3c14ae7df909600056fc36d26a52270862e4
Cas de Reuver <casdr@0fi.net>
2015-06-02 20:29:22 +0200

    asdfhasdif

--- modules/app_iotd.php
+++ modules/app_iotd.php
@@ -8,7 +8,7 @@ class app_iotd {
 		$page = file_get_html('http://photography.nationalgeographic.com/photography/photo-of-the-day/nature-weather/');
 		$link = $page->find('#search_results', 0)->find('a', 0)->href;
 
-		$page = file_get_html($link);
+		$page = file_get_html('http://photography.nationalgeographic.com'.$link);
 		$img = $page->find('.primary_photo', 0)->find('img', 0)->src;
 		return $img;
 	}
