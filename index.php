ea41816898ed13102d3547957f12f5b0b0921fce
Cas de Reuver <casdr@0fi.net>
2015-06-02 20:32:29 +0200

    JSON response

--- index.php
+++ index.php
@@ -113,8 +113,7 @@ $app->post('/v1/student/:id/grades/:periode', function($id, $periode) {
 
 // Stuff for in the app
 $app->get('/v1/app/iotd', function () use($app) {
-  $app->response->headers->set('Location', app_iotd::main());
-  // echo app_iotd::main();
+  createResponse(array('url'=>app_iotd::main());
 });
 $app->get('/v1/app/versions', function () {
 	createResponse(array(
