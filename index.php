2ba55e609d657ee0cb10727c1b2db107fbcff71d
Cas de Reuver <casdr@0fi.net>
2015-06-07 17:42:46 +0000

    Test

--- index.php
+++ index.php
@@ -38,6 +38,24 @@ $app = new \Slim\Slim();
 
 $app->response->headers->set('Access-Control-Allow-Origin', '*');
 
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+    $app->response->headers->set("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+    $app->response->headers->set('Access-Control-Allow-Credentials: true');
+    $app->response->headers->set('Access-Control-Max-Age: 86400');    // cache for 1 day
+}
+
+// Access-Control headers are received during OPTIONS requests
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+        $app->response->headers->set("Access-Control-Allow-Methods: GET, POST, OPTIONS");         
+
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+        $app->response->headers->set("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+    exit(0);
+}
+
 if(isset($_GET['callback'])) echo $_GET['callback'].'(';
 $app->get('/', function () {
   echo 'You shouldn\'t be here!';
